1d29400bbe7fdb6e625c1854fa953651f2a12d6f
tamanium <111425337+tamanium@users.noreply.github.com>
2025-08-05 12:18:46 +0900

    ContentView.swift を更新

--- ContentView.swift
+++ ContentView.swift
@@ -3,10 +3,10 @@ import SwiftUI
 struct ContentView: View {
     // 現在時刻
     @State var nowTime = Date()
-
+    
     // 時刻更新用タイマー
     private let timer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
-
+    
     // 時間のフォーマッター（staticで再利用）
     private static let hourFormatter: DateFormatter = {
         let formatter = DateFormatter()
@@ -14,7 +14,7 @@ struct ContentView: View {
         formatter.locale = Locale(identifier: "en_JP")
         return formatter
     }()
-
+    
     // 分のフォーマッター(staticで再利用)
     private static let minuteFormatter: DateFormatter  = {
         let formatter = DateFormatter()
@@ -24,20 +24,24 @@ struct ContentView: View {
     }()
     
     var body: some View {
-        // タテ配置
-        VStack {
-            // ヨコ配置
-            HStack {
-                Text(Self.hourFormatter.string(from: nowTime))
-                Text(":")
-                Text(self.minuteFormatter.string(from: nowTime))
+        GeometryReader { geometry in
+            // タテ配置
+            VStack {
+                // ヨコ配置
+                HStack(spacing:-1) {
+                    Text(Self.hourFormatter.string(from: nowTime))
+                    Text(":")
+                    Text(Self.minuteFormatter.string(from: nowTime))
+                }
+                .frame(maxWidth: .infinity, alignment: .center)
+                .font(.system(size: geometry.size.width * 0.42, weight: .light, design: .rounded))
+                Image(systemName: "globe")
+                    .imageScale(.large)
+                    .foregroundColor(.accentColor)
+                Text("Hello, world!")
             }
-            .font(.system(size: 30, weight: .light, design: rounded))
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            .frame(maxWidth: .infinity, alignment: .center)
+            .onReceive(timer) {input in nowTime = input}
         }
-        .onReceive(timer) {input in nowTime = input}
     }
 }
