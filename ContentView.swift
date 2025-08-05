3bc31111fc71eb05d86e9c77b2a1842c85f8feec
tamanium <111425337+tamanium@users.noreply.github.com>
2025-08-05 16:48:57 +0900

    Update ContentView.swift

--- ContentView.swift
+++ ContentView.swift
@@ -47,75 +47,77 @@ struct ContentView: View {
 
     // 表示
     var body: some View {
-        GeometryReader { geometry in
-            // タテ配置
-            VStack {
-                // -----時計表示領域-----
-                // ヨコ配置
-                HStack(alignment: .lastTextbaseline, spacing:-1) {
-                    // 時間
-                    Text(Self.hourFormatter.string(from: nowTime))
-                        .font(.system(size: geometry.size.width * 0.42, weight: .light, design: .rounded))
-                    // コロン
-                    Text(":")
-                        .font(.system(size: geometry.size.width * 0.42, weight: .light, design: .rounded))
-                    // 分
-                    Text(Self.minuteFormatter.string(from: nowTime))
-                        .font(.system(size: geometry.size.width * 0.42, weight: .light, design: .rounded))
-                    // 分
-                    Text(Self.secondFormatter.string(from: nowTime))
-                        .font(.system(size: geometry.size.width * 0.2, weight: .light, design: .rounded))
+        NavigationStack {
+            GeometryReader { geometry in
+                // タテ配置
+                VStack {
+                    // -----時計表示領域-----
+                    // ヨコ配置
+                    HStack(alignment: .lastTextbaseline, spacing:-1) {
+                        // 時間
+                        Text(Self.hourFormatter.string(from: nowTime))
+                            .font(.system(size: geometry.size.width * 0.42, weight: .light, design: .rounded))
+                        // コロン
+                        Text(":")
+                            .font(.system(size: geometry.size.width * 0.42, weight: .light, design: .rounded))
+                        // 分
+                        Text(Self.minuteFormatter.string(from: nowTime))
+                            .font(.system(size: geometry.size.width * 0.42, weight: .light, design: .rounded))
+                        // 分
+                        Text(Self.secondFormatter.string(from: nowTime))
+                            .font(.system(size: geometry.size.width * 0.2, weight: .light, design: .rounded))
+                    }
+                    // 幅：親画面いっぱい、中央寄せ
+                    .frame(maxWidth: .infinity, alignment: .center)
+                    // 背景：黒
+                    .background(Color.black)
+    
+                    // -----タイムテーブル領域-----
+                    Grid(alignment: .leading, horizontalSpacing: 16, verticalSpacing: 8) {
+                        GridRow {
+                            Text("時刻").bold()
+                            Text("学校名").bold()
+                        }
+                        Divider().gridCellUnsizedAxes([.horizontal, .vertical])
+    
+                        ForEach(scheduleRows) { row in
+                            GridRow {
+                                Text(row.time)
+                                Text(row.name)
+                            }
+                       }
+                    }
+                    .padding()
+                    .background(Color.black)
+                    .cornerRadius(2)
+                    
+                    // 画像
+                    Image(systemName: "globe")
+                        .imageScale(.large)
+                        .foregroundColor(.accentColor)
+                    // テキスト
+                    Text("Hello, world!")
+                    Spacer()
+    
+                    // 画面遷移ボタン
+                    NavigationLink(destination: SecondView()) {
+                        Text("Data")
+                        .font(.headline)
+                        .foregroundColor(.white)
+                        .padding()
+                        .frame(maxWidth: .infinity)
+                        .background(Color.blue)
+                        .cornerRadius(10)
+                        .padding(.horizontal)
+                    }
                 }
-                // 幅：親画面いっぱい、中央寄せ
-                .frame(maxWidth: .infinity, alignment: .center)
+                // 幅：画面いっぱい
+                .frame(maxheight: geometry.size.height * 0.5, maxWidth: .infinity, alignment: .center)
                 // 背景：黒
                 .background(Color.black)
-
-                // -----タイムテーブル領域-----
-                Grid(alignment: .leading, horizontalSpacing: 16, verticalSpacing: 8) {
-                    GridRow {
-                        Text("時刻").bold()
-                        Text("学校名").bold()
-                    }
-                    Divider().gridCellUnsizedAxes([.horizontal, .vertical])
-
-                    ForEach(scheduleRows) { row in
-                        GridRow {
-                            Text(row.time)
-                            Text(row.name)
-                        }
-                   }
-                }
-                .padding()
-                .background(Color.black)
-                .cornerRadius(2)
-                
-                // 画像
-                Image(systemName: "globe")
-                    .imageScale(.large)
-                    .foregroundColor(.accentColor)
-                // テキスト
-                Text("Hello, world!")
-                Spacer()
-
-                // 画面遷移ボタン
-                NavigationLink(destination: SecondView()) {
-                    Text("Data")
-                    .font(.headline)
-                    .foregroundColor(.white)
-                    .padding()
-                    .frame(maxWidth: .infinity)
-                    .background(Color.blue)
-                    .cornerRadius(10)
-                    .padding(.horizontal)
-                }
+                // 1分ごとに表示時間更新
+                .onReceive(timer) {input in nowTime = input}
             }
-            // 幅：画面いっぱい
-            .frame(maxheight: geometry.size.height * 0.5, maxWidth: .infinity, alignment: .center)
-            // 背景：黒
-            .background(Color.black)
-            // 1分ごとに表示時間更新
-            .onReceive(timer) {input in nowTime = input}
         }
     }
 }
