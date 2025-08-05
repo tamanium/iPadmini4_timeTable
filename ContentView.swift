69e13fd1c5124652bd8ac1e9fac55e36655a163c
tamanium <111425337+tamanium@users.noreply.github.com>
2025-08-05 16:58:34 +0900

    ContentView.swift を更新

--- ContentView.swift
+++ ContentView.swift
@@ -10,7 +10,7 @@ struct ScheduleRow: Identifiable {
 struct ContentView: View {
     // 現在時刻
     @State var nowTime = Date()
-
+    
     // タイムテーブルデータ
     let scheduleRows: [ScheduleRow] = [
         ScheduleRow(time:"12:40", name:"ABC学園"),
@@ -44,7 +44,7 @@ struct ContentView: View {
         formatter.locale = Locale(identifier: "en_JP")
         return formatter
     }()
-
+    
     // 表示
     var body: some View {
         NavigationStack {
@@ -53,25 +53,23 @@ struct ContentView: View {
                 VStack {
                     // -----時計表示領域-----
                     // ヨコ配置
-                    HStack(alignment: .lastTextbaseline, spacing:-1) {
+                    HStack(alignment: .lastTextBaseline, spacing:-1) {
                         // 時間
                         Text(Self.hourFormatter.string(from: nowTime))
-                            .font(.system(size: geometry.size.width * 0.42, weight: .light, design: .rounded))
+                            .font(.system(size: geometry.size.width * 0.4, weight: .light, design: .rounded))
                         // コロン
                         Text(":")
-                            .font(.system(size: geometry.size.width * 0.42, weight: .light, design: .rounded))
+                            .font(.system(size: geometry.size.width * 0.4, weight: .light, design: .rounded))
                         // 分
                         Text(Self.minuteFormatter.string(from: nowTime))
-                            .font(.system(size: geometry.size.width * 0.42, weight: .light, design: .rounded))
-                        // 分
-                        Text(Self.secondFormatter.string(from: nowTime))
-                            .font(.system(size: geometry.size.width * 0.2, weight: .light, design: .rounded))
+                            .font(.system(size: geometry.size.width * 0.4, weight: .light, design: .rounded))
+                     
                     }
                     // 幅：親画面いっぱい、中央寄せ
                     .frame(maxWidth: .infinity, alignment: .center)
                     // 背景：黒
                     .background(Color.black)
-    
+                    
                     // -----タイムテーブル領域-----
                     Grid(alignment: .leading, horizontalSpacing: 16, verticalSpacing: 8) {
                         GridRow {
@@ -79,13 +77,13 @@ struct ContentView: View {
                             Text("学校名").bold()
                         }
                         Divider().gridCellUnsizedAxes([.horizontal, .vertical])
-    
+                        
                         ForEach(scheduleRows) { row in
                             GridRow {
                                 Text(row.time)
                                 Text(row.name)
                             }
-                       }
+                        }
                     }
                     .padding()
                     .background(Color.black)
@@ -98,21 +96,21 @@ struct ContentView: View {
                     // テキスト
                     Text("Hello, world!")
                     Spacer()
-    
+                    
                     // 画面遷移ボタン
                     NavigationLink(destination: SecondView()) {
                         Text("Data")
-                        .font(.headline)
-                        .foregroundColor(.white)
-                        .padding()
-                        .frame(maxWidth: .infinity)
-                        .background(Color.blue)
-                        .cornerRadius(10)
-                        .padding(.horizontal)
+                            .font(.headline)
+                            .foregroundColor(.white)
+                            .padding()
+                            .frame(maxWidth: .infinity)
+                            .background(Color.blue)
+                            .cornerRadius(10)
+                            .padding(.horizontal)
                     }
                 }
                 // 幅：画面いっぱい
-                .frame(maxheight: geometry.size.height * 0.5, maxWidth: .infinity, alignment: .center)
+                .frame(maxWidth: .infinity, maxHeight: geometry.size.height * 0.5, alignment: .center)
                 // 背景：黒
                 .background(Color.black)
                 // 1分ごとに表示時間更新
