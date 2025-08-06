139d2a7dcaa7c7225987b8aec0b2b232225cf51c
tamanium <111425337+tamanium@users.noreply.github.com>
2025-08-06 15:37:20 +0900

    Update Define.swift

--- Define.swift
+++ Define.swift
@@ -2,19 +2,20 @@ import Foundation
 
 // ステータス定義
 enum Status: String, Codable {
-    case home         = "前日"
-    case coming       = "移動中"
-    case checkIn      = "受付"
-    case takingIn     = "楽器搬入"
-    case preparing    = "楽器出し"
-    case beforeTuning = "チューニング待ち"
-    case tuning       = "チューニング"
-    case waiting1     = "待機1"
-    case waiting2     = "待機2"
-    case performing   = "演奏"
-    case putAway      = "楽器片付け"
-    case takingOut    = "楽器搬出"
-    case done         = "完了"
+    
+    case home         = "00" // 前日
+    case coming       = "01" // 移動中
+    case checkIn      = "02" // 受付
+    case takingIn     = "03" // 楽器搬入
+    case preparing    = "04" // 楽器準備
+    case beforeTuning = "05" // チューニング室待機
+    case tuning       = "06" // チューニング
+    case waiting1     = "07" // 待機1
+    case waiting2     = "08" // 待機2
+    case performing   = "09" // 演奏本番
+    case putAway      = "10" // 楽器片付け
+    case takingOut    = "11" // 楽器搬出
+    case done         = "12" // 予定完了
 }
 
 // スケジュール行
