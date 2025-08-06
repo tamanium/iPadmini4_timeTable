ffdd4906fa3966b277d5bf56219fa4fd27475d20
tamanium <111425337+tamanium@users.noreply.github.com>
2025-08-06 14:09:18 +0900

    Update ContentView.swift

--- ContentView.swift
+++ ContentView.swift
@@ -1,30 +1,5 @@
 import SwiftUI
 
-// 状態
-enum Status: String {
-    case home         = "前日"
-    case coming       = "移動中"
-    case checkIn      = "受付"
-    case takingIn     = "楽器搬入"
-    case preparing    = "楽器出し"
-    case beforeTuning = "チューニング待ち"
-    case tuning       = "チューニング"
-    case waiting1     = "待機1"
-    case waiting2     = "待機2"
-    case performing   = "演奏"
-    case putAway      = "楽器片付け"
-    case takingOut    = "楽器搬出"
-    case done         = "完了"
-}
-
-// 構造体：タイムテーブルの行
-struct ScheduleRow: Identifiable {
-    let id = UUID()    // ID
-    let time: String   // 時刻
-    let name: String   // 団体名
-    let status: Status // 状態
-}
-
 struct ContentView: View {
     // 現在時刻
     @State var nowTime = Date()
