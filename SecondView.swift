77ee73f782d4d89071dff546e0afaa262ea36649
tamanium <111425337+tamanium@users.noreply.github.com>
2025-08-12 13:19:36 +0900

    SecondView.swift を更新

--- SecondView.swift
+++ SecondView.swift
@@ -1,17 +1,17 @@
 import SwiftUI
 struct SecondView: View {
-    var row: ScheduleRow
     @ObservedObject var model: ScheduleModel
+    var row: ScheduleRow
     @State private var name: String
     @State private var date: Date
-
+    
     init(model: ScheduleModel, row: ScheduleRow) {
         self.model = model
         self.row = row
         _name = State(initialValue: row.name)
         _date = State(initialValue: row.date)
     }
-
+    
     var body: some View { 
         Form {
             TextField("団体名", text: $name)
@@ -22,12 +22,12 @@ struct SecondView: View {
         }
         .navigationTitle("編集")
         /*
-        VStack { 
-            Text("別画面") .font(.title) .padding() 
-            Spacer() 
-        } 
-        .navigationTitle("別画面タイトル") 
-        .background(Color.black)
-        */
+         VStack { 
+         Text("別画面") .font(.title) .padding() 
+         Spacer() 
+         } 
+         .navigationTitle("別画面タイトル") 
+         .background(Color.black)
+         */
     } 
 }
