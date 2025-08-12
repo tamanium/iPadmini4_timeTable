73637a4fdf7528add90a39e4187b9abfb6f5d78c
tamanium <111425337+tamanium@users.noreply.github.com>
2025-08-12 13:04:38 +0900

    Update ContentView.swift

--- ContentView.swift
+++ ContentView.swift
@@ -132,17 +132,26 @@ struct ContentView: View {
     }
 }
 
+// 行の表示
 struct ScheduleRowView: View {
-    let row: ScheduleRow 
+    let row: ScheduleRow
+    @ObservedObject var model: ScheduleModel
     var body: some View {
         let opacity = Utils.setOpacity(row.nowStatus, base: .performing)
         GridRowView(
             status: row.nowStatus.rawValue,
             time: Utils.formatDate(row.date, format: "HH:mm"), 
-            name: row.name ) 
-        .opacity(opacity)
-        .id(row.id) 
-    } 
+            name: row.name
+        )         Spacer()
+        NavigationLink("編集") {
+            ScheduleEditView(model: model, row: row)
+        }
+        Button("削除") {
+            model.deleteRow(id: row.id)
+        }
+    }
+    .opacity(opacity)
+    .id(row.id) 
 } 
 struct GridRowView: View { 
     let status: String 
