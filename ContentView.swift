2e85fa8a4f0f2cd9b7b93fb25d5ef32f4b03470c
tamanium <111425337+tamanium@users.noreply.github.com>
2025-08-13 17:53:19 +0900

    ContentView.swift を更新

--- ContentView.swift
+++ ContentView.swift
@@ -3,18 +3,14 @@ import SwiftUI
 struct ContentView: View {
     
     @StateObject private var model = ScheduleModel()
-    // 現在時刻
     @State var nowTime = Date()
-    // 時刻更新用タイマー
     private let timer = Timer.publish(every: 1, on: .main, in: .common)
         .autoconnect()
-    // 演奏中の行を基準にスクロール
+    
     @State private var scrollToPerforming: (() -> Void)?
-    // リンク処理
     @State private var path = NavigationPath()
-    // 初回フラグ
     @State private var isInit = false
-    // 表示
+    
     var body: some View {
         NavigationStack(path: $path) {
             GeometryReader { geometry in
@@ -29,13 +25,13 @@ struct ContentView: View {
                     // ヨコ配置
                     HStack(alignment: .lastTextBaseline, spacing: -8) {
                         // 時間
-                        Text(Utils.formatDate(nowTime, format: "HH")).font(timeFont)
+                        Text(Utils.formatDate(model.nowTime, format: "HH")).font(timeFont)
                         // コロン
                         Text(":").font(timeFont)
                         // 分
-                        Text(Utils.formatDate(nowTime, format: "mm")).font(timeFont)
+                        Text(Utils.formatDate(model.nowTime, format: "mm")).font(timeFont)
                         // 秒
-                        Text(Utils.formatDate(nowTime, format: "ss")).font(secondFont)
+                        Text(Utils.formatDate(model.nowTime, format: "ss")).font(secondFont)
                     }
                     .minimumScaleFactor(0.5)    // 最小50%まで縮小
                     .lineLimit(1)               // 折り返し防止
@@ -44,53 +40,58 @@ struct ContentView: View {
                         maxWidth: .infinity, // 幅：親画面幅
                         alignment: .center      // 中央寄せ
                     )
-                    .background(Color.black) 
+                    .background(Color.black) /*
                     .onReceive(timer) {input in // 1秒ごとに表示時間更新
                         nowTime = input
-                    }
+                    }*/
                     // -------タイムテーブル領域--------
                     // データテーブル領域
                     VStack(spacing: 0){
                         // ヘッダ行
                         Text("演奏時刻")
-                        // ダブルタップのイベント
-                            .onTapGesture(count:1) {
-                                // 演奏中の行を基準にスクロール
-                                scrollToPerforming?()
+                        // タップのイベント
+                        /*
+                            .gesture(){
+                                print("title tap")
+                                DispatchQueue.main.async{
+                                    withAnimation {
+                                        scrollProxy.scrollTo(scrollID, anchor: .top)
+                                    }
+                                }
                             }
+                         */
                         // スクロール領域
                         ScrollViewReader { scrollProxy in
                             // 縦スクロール領域
                             ScrollView(.vertical) {
                                 Grid(alignment: .leading, horizontalSpacing: 32, verticalSpacing: 16){
                                     // 表示
-                                    ForEach(model.scheduleRows) { row in
+                                    ForEach(model.scheduleRows, id: \.id) { row in
                                         ScheduleRowView(row: row, model: model)
                                     }
                                 }
-                                // タイマーイベント
-                                .onReceive(timer) { currentTime in
-                                    if let scrollID = model.updateStatuses(currentTime: currentTime) {
-                                        withAnimation {
-                                            scrollProxy.scrollTo(scrollID, anchor: .top)
+                                // ScrollView内
+                                .onChange(of: model.nowTime) {
+                                    if let scrollID = model.updateStatuses(currentTime: model.nowTime) {
+                                        print("scrollID: ", scrollID)
+                                        DispatchQueue.main.async{
+                                            withAnimation {
+                                                scrollProxy.scrollTo(scrollID, anchor: .top)
+                                            }
                                         }
                                     }
                                 }
+
                                 .padding(.bottom, geometry.size.height * 0.5)
                             }
                             .onAppear{
                                 model.scrollToPerforming = {
-                                    // 演奏中の行を検索
-                                    let topID: AnyHashable
-                                    var topIndex = 0
-                                    // あれば演奏中行の1つ上の行のインデックスを取得
-                                    if let index = model.scheduleRows.firstIndex(where: {$0.nowStatus == .performing}){
-                                        topIndex = max(0, index-1)
-                                    }
-                                    topID = model.scheduleRows[topIndex].id
-                                    // スクロール処理
-                                    withAnimation(.easeInOut(duration: 0.5)) {
-                                        scrollProxy.scrollTo(topID, anchor: .top)
+                                    // 最上位行IDを取得
+                                    if let topID = model.getTopIdByStatus(.performing){
+                                        // スクロール処理
+                                        withAnimation(.easeInOut(duration: 0.5)) {
+                                            scrollProxy.scrollTo(topID, anchor: .top)
+                                        }
                                     }
                                 }
                             }
@@ -154,39 +155,14 @@ struct ScheduleRowView: View {
     var body: some View {
         let opacity = Utils.setOpacity(row.nowStatus, base: .performing)
         HStack {
-            //Spacer()
             GridRowView(
                 status: row.nowStatus.rawValue,
                 time: Utils.formatDate(row.date, format: "HH:mm"), 
                 name: row.name
-            )/*
-            Spacer()
-            HStack {
-                Button("編集") {
-                    newName = row.name
-                    newDate = row.date
-                    isEditMode = true
-                }
-                Button("削除") {
-                    model.deleteRow(id: row.id)
-                }
-            }*/
+            )
         }
         .opacity(opacity)
-        .id(row.id) /*
-        .sheet(isPresented: $isEditMode) {
-            EditSheet(
-                name: $newName,
-                date: $newDate,
-                onSave: {
-                    model.updateRow(id: row.id, name: newName, date: newDate)
-                    isEditMode = false
-                },
-                onCancel: {
-                    isEditMode = false
-                }
-            )
-        }*/
+        .id(row.id)
     }
 }
 
