72c669852765d9bab2ef628e4ff661b82aa7cc14
tamanium <111425337+tamanium@users.noreply.github.com>
2025-08-14 15:20:08 +0900

    Update and rename SecondView.swift to EditView.swift

--- EditView.swift
+++ EditView.swift
@@ -1,6 +1,6 @@
 import SwiftUI
 
-struct ScheduleEditView: View {
+struct EditView: View {
     @ObservedObject var model: ScheduleModel
     @Environment(\.dismiss) var dismiss
     @State private var editedRows: [EditableRow] = []
