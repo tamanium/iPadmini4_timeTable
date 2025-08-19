8a9fa94308cbf7258ffafd505bed7b1aff932b4d
tamanium <111425337+tamanium@users.noreply.github.com>
2025-08-19 13:14:48 +0900

    Update ViewModel.swift

--- ViewModel.swift
+++ ViewModel.swift
@@ -74,11 +74,11 @@ class ViewModel: ObservableObject {
      }*/
     
     // ステータス更新・最上位行ID取得
-    func updateStatuses(status: Status, currentTime: Date) -> UUID? {
+    func updateStatuses(stdStatus: Status, currentTime: Date) -> UUID? {
         var scrollID: UUID?
         
         for i in schedules.indices {
-            guard let date = schedules[i].statusDates?[status] else { continue }
+            guard let date = schedules[i].statusDates?[stdStatus] else { continue }
             // 経過した行をdoneにする
             if date <= currentTime {
                 schedules[i].setStatus(.done)
@@ -86,7 +86,7 @@ class ViewModel: ObservableObject {
             } else {
                 // 直前の行を performing にする
                 if i > 0 {
-                    schedules[i-1].setStatus(status)
+                    schedules[i-1].setStatus(stdStatus)
                 } 
                 // 最上位行IDを取得
                 let topIndex = max(0, i - (i > 1 ? 2 : 1))
