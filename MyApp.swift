73b8ebe426a09ebe05421ce99ee1430f93bec13e
tamanium <111425337+tamanium@users.noreply.github.com>
2025-08-19 13:56:59 +0900

    Update MyApp.swift

--- MyApp.swift
+++ MyApp.swift
@@ -2,6 +2,12 @@ import SwiftUI
 
 @main
 struct MyApp: App {
+
+    init() {
+        // アプリ起動時にスリープを無効にする
+        UIApplication.shared.isIdleTimerDisabled = true
+    }
+
     var body: some Scene {
         WindowGroup {
             MainView()
