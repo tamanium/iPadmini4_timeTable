22593a555c7a3703417407adebd59d4fbd1ee2be
tamanium <111425337+tamanium@users.noreply.github.com>
2025-08-19 15:28:14 +0900

    Update EditView.swift

--- EditView.swift
+++ EditView.swift
@@ -4,6 +4,7 @@ struct EditView: View {
     @ObservedObject var vm: ViewModel
     @Environment(\.dismiss) var dismiss
     @State private var editedRows: [EditableRow] = []
+    @State private var isSwapped = false;
     
     struct EditableRow: Identifiable {
         let id: UUID
@@ -16,21 +17,42 @@ struct EditView: View {
             Text("全体編集")
                 .font(.title)
                 .padding()
+            Button("←→") {
+                //withAnimation(.easeInOut(duration:0.5)) {
+                    isSwapped.toggle()
+                //}
+            }
             ScrollView {
                 Grid(alignment: .leading, horizontalSpacing: 32, verticalSpacing: 16) {
                     ForEach($editedRows) { $row in
                         GridRow {
-                            TextField("時刻", text: $row.timeString)
-                                .font(.system(size: 50, design: .monospaced)) 
-                                .keyboardType(.numberPad)
-                                .frame(width: 180)
-                                .textFieldStyle(.roundedBorder)
-                            
-                            TextField("団体名", text: $row.name)
-                                .font(.system(size:50)) 
-                                .frame(width: 450)
-                                .textFieldStyle(.roundedBorder)
+                            if !isSwapped {
+                                TextField("時刻", text: $row.timeString)
+                                    .font(.system(size: 50, design: .monospaced)) 
+                                    .keyboardType(.numberPad)
+                                    .frame(width: 180)
+                                    .textFieldStyle(.roundedBorder)
+                                    //.transition(.move(edge: .leading))
+                                TextField("団体名", text: $row.name)
+                                    .font(.system(size:50)) 
+                                    .frame(width: 450)
+                                    .textFieldStyle(.roundedBorder)
+                                    //.transition(.move(edge: .trailing))
+                            } else {
+                                TextField("団体名", text: $row.name)
+                                    .font(.system(size:50)) 
+                                    .frame(width: 450)
+                                    .textFieldStyle(.roundedBorder)
+                                    //.transition(.move(edge: .leading))
+                                TextField("時刻", text: $row.timeString)
+                                    .font(.system(size: 50, design: .monospaced)) 
+                                    .keyboardType(.numberPad)
+                                    .frame(width: 180)
+                                    .textFieldStyle(.roundedBorder)
+                                    //.transition(.move(edge: .trailing))
+                            }
                         }
+                        //.animation(.easeInOut(duration: 0.5), value: isSwapped)
                     }
                 }
                 .padding()
