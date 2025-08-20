89e2c1e9667f136829d8494e3726de049fcede93
tamanium <111425337+tamanium@users.noreply.github.com>
2025-08-20 15:57:52 +0900

    Update Utils.swift

--- Utils.swift
+++ Utils.swift
@@ -32,7 +32,28 @@ struct Utils {
             return 0.3
         }
     }
-    
+    // 時分比較
+    static func compareHHmm(_ date1: Date, _ date2: Date) -> ComparisonResult {
+        let calendar = Calendar.current
+        let comp1 = calendar.dateCompornents([.hour, .minute], from: date1)
+        let comp2 = calendar.dateCompornents([.hour, .minute], from: date2)
+
+        if let hour1 = comp1.hour, let min1 = comp1.minute, let hour2 = comp2.hour, let min2 = comp.minute {
+            if hour1 < hour2 || (hour1 == hour2 && minute1 < minute2) {
+                // 左 < 右 の場合
+                return .orderdAscending
+            } else if hour1 == hour2 && minute1 == minute2 {
+                // 左 == 右の場合
+                return .orderedSame
+            } else {
+                // 左 > 右 の場合
+                return .orderdDescending
+            }
+        }
+        // 比較不可の場合は同じとする
+        return .orderedSame
+    }
+    // カウント値から時分を算出（デバッグ用）
     static func parseHHmm(_ string: String) -> Date? {
         guard string.count == 4,
               let hour = Int(string.prefix(2)),
